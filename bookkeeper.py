6f001af72e71b0634612508d037396d5df7764c6
lNsufficient <edvard.jo@gmail.com>
2021-01-04 14:21:12 +0100

    Class ImportCSVBank

--- bookkeeper.py
+++ bookkeeper.py
@@ -16,13 +16,39 @@ class Bookkeeper:
         """
         pass
 
+
     def month_summary(self, month):
         pass
 
     def classify_transaction(self):
         pass
 
-class Swedbank(Bookkeeper):
+class ImportCSVBank(Bookkeeper):
+    import_columns = []
+    date_columns = [] #Columns to find datettime.
+    column_rename_dict = {
+    }
+
+    separator = ","
+    thousands = " " #Thousands separator
+    decimal = "," #Decimal separator
+    quotechar = '"'
+    
+    encoding = "utf-8"
+
+    def read_csv(self, csv_path):
+        return pd.read_csv(
+            csv_path, 
+            delimiter=self.separator, 
+            index_col=False,
+            header=1,
+            parse_dates=self.date_columns,
+            decimal=self.decimal,
+            quotechar=self.quotechar,
+            encoding=self.encoding,
+            )
+
+class Swedbank(ImportCSVBank):
     separator = ','
     import_columns = "Radnummer,Clearingnummer,Kontonummer,Produkt,Valuta,Bokföringsdag,Transaktionsdag,Valutadag,Referens,Beskrivning,Belopp,Bokfört saldo"
     import_columns = import_columns.split(separator)
@@ -35,9 +61,10 @@ class Swedbank(Bookkeeper):
         "Bokföringsdag":"date",  
         "Belopp":"amount"
     }
+    encoding = "ISO-8859-1"
 
     def get_account(self, x):
-        return str(x["Clearingnummer"]) + "-" + str(x["Kontonummer"])
+        return str(x["Clearingnummer"]) + ", " + str(x["Kontonummer"])
 
     def get_note(self, x):
         note = x["Beskrivning"]
@@ -46,19 +73,7 @@ class Swedbank(Bookkeeper):
         return note 
 
     def import_transactions(self, csv_path):
-                
-        new_df = pd.read_csv(
-            csv_path, 
-            delimiter=self.separator, 
-            index_col=False,
-            header=1,
-            parse_dates=self.date_columns,
-            decimal=self.decimal,
-            quotechar=self.quotechar,
-            encoding="ISO-8859-1",
-            )
-        print(new_df.head())
-        
+        new_df = self.read_csv(csv_path)
         new_df["account"] = new_df.apply(func=self.get_account, axis = "columns")
         new_df["note"] = new_df.apply(func=self.get_note, axis="columns")
         new_df.rename(columns=self.column_rename_dict, inplace=True)
@@ -66,5 +81,3 @@ class Swedbank(Bookkeeper):
         new_df = new_df[new_df_columns]
         self.latest_import_df = new_df
         return self.latest_import_df
-
-    
