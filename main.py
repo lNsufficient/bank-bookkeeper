f3fa073f7380244e02aa8b14540734cb1232fa8c
lNsufficient <edvard.jo@gmail.com>
2021-01-04 16:57:56 +0100

    add more test data

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from sys import argv
 import bookkeeper as bk
 
 if __name__ == "__main__":
+    bookkeeper = bk.Bookkeeper("AllAccounts")
     transaction_account = bk.Swedbank("privatkonto")
-    transaction_account.import_transactions(argv[1])
-    print(transaction_account.latest_import_df.head())
+    privatkonto_df = transaction_account.import_transactions(argv[1])
+    bookkeeper.add_transactions(privatkonto_df)
+
+    e_account = bk.Swedbank("e-konto")
+    e_df = e_account.import_transactions(argv[2])
+    bookkeeper.add_transactions(e_df)
+
+    bookkeeper.summary()
